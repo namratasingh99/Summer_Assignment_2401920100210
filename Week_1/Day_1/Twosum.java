dad0c72562d2ff35a688784679a4fc765edbbd00
namratasingh99 <namsinghrajpoot9@gmail.com>
2026-06-01 10:11:30 +0530

    TwoSum

---
+++ Week_1/Day_1/Twosum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+         int[] result = new int [2];
+        int n = nums.length;
+      outer: 
+       for (int i=0 ; i<n ; i++){
+            for (int j = i+1 ; j<n ; j++){
+                if ((nums[i]+nums[j])==target){
+                    result[0]=i;
+                    result[1]=j;
+                    break outer;
+                }
+            }
+       }
+       return result;
+    }
+}

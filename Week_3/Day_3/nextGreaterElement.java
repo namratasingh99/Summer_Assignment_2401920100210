c5e32c52d4dbd0aeeae8c1fe943462244f1baa2d
namratasingh99 <namsinghrajpoot9@gmail.com>
2026-06-18 22:53:55 +0530

    Implement nextGreaterElement function

---
+++ Week_3/Day_3/nextGreaterElement.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] ans = new int[10001];
+        Stack<Integer> stack = new Stack<>();
+        for(int i= nums2.length-1 ; i>=0 ; i--){
+            while(!stack.isEmpty() && stack.peek() <= nums2[i])
+                stack.pop();
+            ans[nums2[i]] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(nums2[i]);
+        }
+        for (int i = 0; i < nums1.length; i++) {
+            nums1[i] = ans[nums1[i]];
+        }
+        return nums1;
+    }
+}
